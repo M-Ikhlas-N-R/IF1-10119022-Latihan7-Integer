c0f3ef2e766a6964d8ad73677749e4441518c234
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-10-16 09:23:44 +0700

    Add files via upload

---
+++ src/LatihanInteger.java
@@ -0,0 +1,24 @@
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Latihan menggunakan Integer
+ *
+ */
+public class LatihanInteger {
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 3276;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte :" + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+}
